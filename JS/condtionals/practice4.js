6f4bb1ae0c9fca8ec9eb4eb46f4ad4540279e81c
shivamdevloper123 <shivam71066@gmail.com>
2025-03-29 16:49:52 +0530

    practice on prototype in JS

--- JS/condtionals/practice4.js
+++ JS/condtionals/practice4.js
@@ -9,3 +9,186 @@
 // greet('Rahul').describe
 
 
+// function Animal(name) {
+//     // Initialize name property
+//  this.name = name
+// } 
+
+
+// Animal.prototype.makeSound = function(){
+  
+//     const sound = "Some generic sound"
+//     return sound
+  
+// }
+
+// const myAnimal= new Animal("rahul")
+// // console.log(myAnimal.makeSound())
+
+//Robot charging Problem statement Solution
+
+// function Robot(name, batteryLevel) {
+//     // Initialize name and batteryLevel properties
+//     this.name = name
+//     this.batteryLevel = batteryLevel
+// }
+// // Define charge method on Robot's prototype
+// Robot.prototype.charge = function(){
+//   if( this.batteryLevel>=100){
+//   return this.batteryLevel
+//   }else if(this.batteryLevel<=80){
+//     return this.batteryLevel +=20
+//   }
+  
+//   else if(this.batteryLevel>=80 ){
+//     for (let i = 1; i<=20 ; i++) {
+
+//       console.log( this.batteryLevel++)
+//         if (this.batteryLevel==100) {
+//             break;
+//         }
+//     }
+//     return this.batteryLevel
+//   }
+// }
+
+// const myRobot= new Robot("robot1",30)
+// console.log(myRobot.charge())
+
+//Counter Problem statement Solution
+// function Counter() {
+//     // Initialize count property
+//     this.count = 0
+//   }
+  
+//   // Define increment method on Counter's prototype
+//   Counter.prototype.increment = function () {
+// this.count+=1
+  
+//   }
+//   Counter.prototype.decrement = function () {
+//     this.count-=1
+//   }
+  
+// const myCount1 = new Counter("increment")
+// console.log(myCount1)
+// const myCount2 = new Counter("decrement")
+// console.log(myCount2)
+
+//Making a song Playlist Problem statement Solution
+
+function Playlist() {
+    // Initialize songs property
+
+    this.songs =[]
+}
+
+// Define addSong method on Playlist's prototype
+Playlist.prototype.addSong = function(song){
+
+ this.songs.push(song)
+}
+const myPlaylist = new Playlist()
+myPlaylist.addSong("tum hi ho na")
+myPlaylist.addSong("kal ho na ho")
+myPlaylist.addSong(" teri diwani ")
+// console.log(myPlaylist.songs)
+
+//Shopping items Problem statement Solution
+
+function ShoppingCart() {
+    // Initialize items property
+    this.items = []
+  }
+  // Define addItem method on ShoppingCart's prototype
+  ShoppingCart.prototype.addItem = function (...price) {
+
+    this.items.push(...price)
+  }
+  
+  // Define getTotalPrice method on ShoppingCart's prototype
+  ShoppingCart.prototype.getTotalPrice = function () {
+  
+   return this.items.reduce((a,price)=>a+price, 0)
+  }
+  
+  const myShopping = new ShoppingCart()
+
+  myShopping.addItem(10,20,30)
+
+
+//   console.log("items :",myShopping.items)
+//  console.log(myShopping.getTotalPrice())
+
+
+//Bank Problem statement Solution
+
+
+function BankAccount(balance) {
+    // Initialize balance and transactions properties
+    this.balance = balance
+    this.transactions = []
+}
+
+// Define deposit method on BankAccount's prototype
+
+BankAccount.prototype.deposit = function(amount){
+this.balance += amount
+ this.transactions.push(`Deposited ${amount}`)
+
+}
+// Define withdraw method on BankAccount's prototype
+BankAccount.prototype.withdraw = function(amount){
+
+if(this.balance>=amount){
+    this.balance -= amount
+    this.transactions.push(`Withdraw  ${amount}`)
+   
+}else{
+     this.transactions.push( "Insufficient balance")
+   
+  }
+}
+// Define getTransactionHistory method on BankAccount's prototype
+BankAccount.prototype.getTransactionHistory = function(){
+ return this.transactions
+
+}
+const myAccount = new BankAccount(200)
+myAccount.deposit(100)
+
+// myAccount.withdraw(50)
+
+
+
+// console.log(myAccount.getTransactionHistory())
+
+function Employee(name, position, salary) {
+    // Initialize name, position, and salary properties
+    this.name = name
+    this.position = position
+    this.salary = salary
+}
+
+// Define applyBonus method on Employee's prototype
+
+function Library() {
+    // Initialize books property
+    this.books = []
+}
+
+// Define addBook method on Library's prototype
+Library.prototype.addBook = function(book){
+return this.books.push(book)
+}
+// Define findBook method on Library's prototype
+Library.prototype.findBook = function(title){
+ return this.books.includes(title)?"Book found":"Book not found";
+  
+}
+
+const myBook = new Library()
+
+console.log(myBook.addBook("Wings"))
+
+console.log(myBook.findBook("Wings"))
